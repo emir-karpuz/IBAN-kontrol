f59cafa0e5daa86cf532785251702aea38d18be4
emir-karpuz <emirkarpuz.megayazilim@outlook.com>
2021-06-28 14:06:00 +0300

    IBAN parçalama formülü uygulandı ve program tamamlandı.

--- IBANKontrol/IBANKontrol.vb
+++ IBANKontrol/IBANKontrol.vb
@@ -1,35 +1,50 @@
 ﻿Module IBANKontrol
 
     Sub Main()
-        IBANKontrol("TR47 0000 1001 0000 0350 9300 01")
+        IBANKontrol("TR130006219932988256155416")
         Console.ReadLine()
     End Sub
 
     Function IBANKontrol(IBAN As String) As Boolean
         Console.WriteLine("IBAN: " & IBAN)
 
+        '! IBAN SAYISI BOŞLUKLARDAN ARINDIRILDI.
         If IBAN.Count > 26 Then
             IBAN = IBAN.Replace(" ", String.Empty)  'TR470000100100000350930001
         End If
         Console.WriteLine("1. Aşama: " & IBAN)
 
-        Dim ParcalanmisIBAN As String() = New String(2) {}
+        '! TR47 SONA ALINDI.
+        Dim ParcalanmisIBAN As String() = New String(4) {}
         ParcalanmisIBAN(0) = IBAN.Substring(0, 4)
         ParcalanmisIBAN(1) = IBAN.Substring(4, 22)
         IBAN = ParcalanmisIBAN(1) & ParcalanmisIBAN(0)
         Console.WriteLine("2. Aşama: " & IBAN)
 
+        '! T VE R DEĞERLERİ SAYISAL DEĞERE ÇEVRİLDİ.
         IBAN = IBAN.Replace("T", "29")
         IBAN = IBAN.Replace("R", "27")
         Console.WriteLine("3. Aşama: " & IBAN)
 
-        ParcalanmisIBAN(0) = IBAN.Substring(0, 9)   '000010010
-        ParcalanmisIBAN(1) = IBAN.Substring(9, 19)  '0000350930001292747
-        ParcalanmisIBAN(0) = ParcalanmisIBAN(0) Mod 97  '19
-        ParcalanmisIBAN(1) = ParcalanmisIBAN(0) & ParcalanmisIBAN(1).Substring(0, 9)    '19000035093
-        ParcalanmisIBAN(0) = ParcalanmisIBAN(1) Mod 97  '43
-        ParcalanmisIBAN(1) = ParcalanmisIBAN(0) & ParcalanmisIBAN(1).Substring(0, 9)    'DÜZENLENECEK
+        '! IBAN PARÇALANDI.
+        ParcalanmisIBAN(0) = IBAN.Substring(0, 9)
+        ParcalanmisIBAN(1) = IBAN.Substring(9, 7)
+        ParcalanmisIBAN(2) = IBAN.Substring(16, 7)
+        ParcalanmisIBAN(3) = IBAN.Substring(23, 5)
 
+        ParcalanmisIBAN(0) = ParcalanmisIBAN(0) Mod 97  '19
+        ParcalanmisIBAN(1) = ParcalanmisIBAN(0) & ParcalanmisIBAN(1)
+        ParcalanmisIBAN(1) = ParcalanmisIBAN(1) Mod 97  '48
+        ParcalanmisIBAN(2) = ParcalanmisIBAN(1) & ParcalanmisIBAN(2)
+        ParcalanmisIBAN(2) = ParcalanmisIBAN(2) Mod 97  '2
+        ParcalanmisIBAN(3) = ParcalanmisIBAN(2) & ParcalanmisIBAN(3)
+        ParcalanmisIBAN(3) = ParcalanmisIBAN(3) Mod 97  '1
+
+        If ParcalanmisIBAN(3) = 1 Then
+            Console.WriteLine("IBAN Doğru")
+        Else
+            Console.WriteLine("IBAN Hatalı")
+        End If
 
         Return False
     End Function
